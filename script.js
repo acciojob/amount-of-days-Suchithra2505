50a64007d2a4be7df95dfb2f21c989a318c649ff
Suchithra2505 <147540982+Suchithra2505@users.noreply.github.com>
2023-12-08 20:03:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function daysOfAYear(year) {
+    // Check if the year is a leap year
+    // Leap year is divisible by 4, but century years must be divisible by 400
+    const isLeapYear = (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
+
+    // Return the number of days based on leap year or not
+    return isLeapYear ? 366 : 365;
+}
+
+
+console.log(alert((daysOfAYear(prompt())))); 
